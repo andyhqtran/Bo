ae5d7ee5e065cd876b90be3e44821e577d4cb80d
Andy Tran <andyhqtran@gmail.com>
2015-05-18 00:27:40 -0700

    Added in template structure.

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: false,
+          yuicompress: true,
+          optimization: 2,
+          sourceMap: false
+        },
+        files: {
+          "assets/css/style.css": "assets/less/style.less",
+          "assets/css/header.css": "assets/less/ui.less",
+          "assets/css/header.css": "assets/less/navigation.less",
+          "assets/css/header.css": "assets/less/header.less"
+        }
+      }
+    },
+    haml: {
+      dist: {
+        files: {
+          'index.html': 'index.haml'
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['assets/less/*.less', '*.haml'],
+        tasks: ['less', 'haml'],
+        options: {
+          nospawn: true,
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-haml');
+
+  grunt.registerTask('default', ['less', 'haml', 'watch']);
+};
